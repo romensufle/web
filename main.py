a009cef6104c1f6f711e355ecb35d95b1cf1868d
sufleee <roman@suftin.ru>
2025-02-14 19:19:59 +0300

    def load photo

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from flask import Flask
+from flask import Flask, request, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def base():
-    return '''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Миссия Колонизация Марса</title>
-                  </head>
-                  <body>
-                    <h1>Миссия Колонизация Марса</h1>
-                  </body>
-                </html>'''
+    return render_template('base.html', title='Миссия Колонизация Марса')
 
 
 @app.route('/index')
@@ -42,17 +33,93 @@ def promo():
 
 @app.route('/image_mars')
 def mars_image():
-    return '''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="static//марс.webp" alt"Mars">
-                  </body>
-                </html>'''
+    return render_template('image_mars.html')
+
+
+@app.route('/form_sample', methods=['POST', 'GET'])
+def form_sample():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример формы</title>
+                          </head>
+                          <body>
+                            <h1>Форма для регистрации в суперсекретной системе</h1>
+                            <div>
+                                <form class="login_form" method="post">
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                                    <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
+                                    <div class="form-group">
+                                        <label for="classSelect">В каком вы классе</label>
+                                        <select class="form-control" id="classSelect" name="class">
+                                          <option>7</option>
+                                          <option>8</option>
+                                          <option>9</option>
+                                          <option>10</option>
+                                          <option>11</option>
+                                        </select>
+                                     </div>
+                                    <div class="form-group">
+                                        <label for="about">Немного о себе</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готов быть добровольцем</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Записаться</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        return f"""{request.form['email'] +
+                    request.form['password'] +
+                    request.form['class'] +
+                    request.form['file'] +
+                    request.form['about'] +
+                    request.form['accept'] +
+                    request.form['sex']
+        }"""
+
+
+@app.route('/load_photo', methods=['POST', 'GET'])
+def load_foto():
+
+    if request.method == 'GET':
+        return render_template('load_photo.html')
+    elif request.method == 'POST':
+        photo = request.files['file']
+        with open('static/img/img.jpg', mode='wb') as f:
+            f.write(photo.read())
+        return render_template('show_photo.html')
 
 
 if __name__ == '__main__':
