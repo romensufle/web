0e380009339682d2af8150657251688162a93f78
sufleee <roman@suftin.ru>
2025-02-14 20:05:52 +0300

    def training

--- main.py
+++ main.py
@@ -122,5 +122,10 @@ def load_foto():
         return render_template('show_photo.html')
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
